318adaf8b0a04655b14fb1e5b88e6c1a163e19e3
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-08-23 12:49:30 +0100

    Code updated

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, relationship
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
